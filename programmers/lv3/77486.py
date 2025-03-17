0085ea8c34f1149cfbba2bff7f3b697da25cfaa9
lvalentine6 <ysr1227@naver.com>
2025-03-17 23:15:53 +0900

    feat: 다단계 칫솔 판매

--- programmers/lv3/77486.py
+++ programmers/lv3/77486.py
@@ -1,27 +1,29 @@
-import sys
-
-sys.setrecursionlimit(1000000)
-
-
-def dfs(amounts, graph, name, money):
-    if name == "-":
-        return
-
-    fee = money // 10
-    amounts[name] += money - fee
-
-    dfs(amounts, graph, graph[name], fee)
-
-
 def solution(enroll, referral, seller, amount):
-    answer = [0 for _ in enroll]
+    answer = []
 
-    graph = {e: referral[idx] for idx, e in enumerate(enroll)}
-    amounts = {e: 0 for e in enroll}
+    graph = {e: idx for idx, e in enumerate(enroll)}
+    parent = [-1 for _ in range(len(enroll))]
+    amounts = [0 for _ in range(len(enroll))]
 
-    for idx, s in enumerate(seller):
-        dfs(amounts, graph, s, amount[idx] * 100)
+    for idx, name in enumerate(referral):
+        if name != "-":
+            parent[idx] = graph[name]
 
-    answer = list(amounts.values())
+    for i in range(len(seller)):
+        name = seller[i]
+        profit = amount[i] * 100
+
+        while name != "-" and profit > 0:
+            idx = graph[name]
+            fee = profit // 10
+            amounts[idx] += profit - fee
+
+            if parent[idx] == -1:
+                break
+
+            name = enroll[parent[idx]]
+            profit = fee
+
+    answer = amounts
 
     return answer
