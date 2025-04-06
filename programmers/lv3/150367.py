e3a205560b28e079ef8b77dea935d446cdf3e799
lvalentine6 <ysr1227@naver.com>
2025-04-07 01:12:53 +0900

    feat: 표현 가능한 이진트리

--- programmers/lv3/150367.py
+++ programmers/lv3/150367.py
@@ -1,3 +1,37 @@
+def search(tree):
+    if len(tree) == 1:
+        return True
+
+    mid = len(tree) // 2
+    root = tree[mid]
+
+    left = tree[:mid]
+    right = tree[mid + 1:]
+
+    if root == '0' and ('1' in left or '1' in right):
+        return False
+
+    return search(left) and search(right)
+
+
 def solution(numbers):
     answer = []
+
+    for n in numbers:
+        bi = format(n, 'b')
+        l = len(bi)
+
+        i = 0
+        while 2 ** i - 1 < l:
+            i += 1
+
+        min_size = 2 ** i - 1
+
+        padded_bi = bi.zfill(min_size)
+
+        if search(padded_bi):
+            answer.append(1)
+        else:
+            answer.append(0)
+
     return answer
