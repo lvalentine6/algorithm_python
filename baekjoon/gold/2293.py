f4d121835a1161c17124868bacc1c5eb2dcba1a8
lvalentine6 <ysr1227@naver.com>
2025-04-11 18:19:52 +0900

    feat: 동전 1

---
+++ baekjoon/gold/2293.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = list(map(int, input().split()))
+lst = [int(input()) for _ in range(n)]
+answer = 0
+
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+
+for coin in lst:
+    for i in range(coin, k + 1):
+       dp[i] += dp[i - coin]
+
+print(dp[k])
+
