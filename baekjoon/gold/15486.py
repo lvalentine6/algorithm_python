d55fbeec9e30e831ab2ad864a4f4551186217370
lvalentine6 <ysr1227@naver.com>
2025-04-18 02:02:00 +0900

    feat: 퇴사 2

--- baekjoon/gold/15486.py
+++ baekjoon/gold/15486.py
@@ -5,6 +5,11 @@ n = int(input())
 calendar = [list(map(int, input().split())) for _ in range(n)]
 dp = [0 for _ in range(n + 1)]
 
+for i in range(n):
+    p, m = calendar[i]
+    dp[i + 1] = max(dp[i], dp[i + 1])
+    if i + p > n:
+        continue
+    dp[i + p] = max(dp[i + p], dp[i] + m)
 
-
-print(calendar)
+print(max(dp))
