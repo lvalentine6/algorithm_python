6794dfdb077a6b9f361a862532175375c94da652
lvalentine6 <ysr1227@naver.com>
2025-04-29 11:40:53 +0300

    불

--- baekjoon/gold/4179.py
+++ baekjoon/gold/4179.py
@@ -2,26 +2,29 @@ import sys
 from collections import deque
 
 
-def fire_extend(board, f_board, fire_lst):
-    moved_fire = []
+def fire_extend(board, f_board, f_visited, start):
+    queue = deque()
+    for s in start:
+        queue.append(s)
+        f_visited[s[0]][s[1]] = True
 
-    for f in fire_lst:
+    while queue:
+        pr, pc, t = queue.popleft()
         for i in range(4):
-            nr = f[0] + dr[i]
-            nc = f[1] + dc[i]
+            nr = pr + dr[i]
+            nc = pc + dc[i]
 
             if 0 <= nr < r and 0 <= nc < c:
-                if board[nr][nc] != '#':
-                    board[nr][nc] = 'F'
-                    moved_fire.append((nr, nc))
+                if not f_visited[nr][nc] and board[nr][nc] != '#':
+                    f_visited[nr][nc] = True
+                    f_board[nr][nc] = t
+                    queue.append((nr, nc, t + 1))
 
-    return moved_fire
 
-def bfs(board, start, visited, fire_lst):
+def bfs(board, f_board, visited, start):
     queue = deque()
     queue.append(start)
     visited[start[0]][start[1]] = True
-    result = 0
 
     while queue:
         pr, pc, time = queue.popleft()
@@ -29,25 +32,25 @@ def bfs(board, start, visited, fire_lst):
             nr = pr + dr[i]
             nc = pc + dc[i]
 
-            if 0 <= nr < r and 0 <= nc < c:
-                if board[nr][nc] == '.':
+            if nr < 0 or nr >= r or nc < 0 or nc >= c:
+                print(time)
+                exit()
+
+            if board[nr][nc] == '.' and not visited[nr][nc]:
+                if f_board[nr][nc] == -1 or time < f_board[nr][nc]:
                     visited[nr][nc] = True
                     queue.append((nr, nc, time + 1))
-            else:
-                return time
-
-    return result
 
 
 input = sys.stdin.readline
 global r, c
 r, c = list(map(int, input().split()))
-start, fired = (), ()
+j_start, f_start = (), []
 board = [list(input().strip()) for _ in range(r)]
-j_board = [[-1 for _ in range(r)] for _ in range(c)]
-f_board = [[-1 for _ in range(r)] for _ in range(c)]
+f_board = [[-1 for _ in range(c)] for _ in range(r)]
 
-visited = [[False for _ in range(c)] for _ in range(r)]
+j_visited = [[False for _ in range(c)] for _ in range(r)]
+f_visited = [[False for _ in range(c)] for _ in range(r)]
 global dr, dc
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
@@ -55,11 +58,11 @@ dc = [0, 0, 1, -1]
 for i in range(r):
     for j in range(c):
         if board[i][j] == 'J':
-            start = (i, j, 1)
+            j_start = (i, j, 1)
         elif board[i][j] == 'F':
-            fired = (i, j)
+            f_start.append((i, j, 1))
 
-fire_extend()
+fire_extend(board, f_board, f_visited, f_start)
+bfs(board, f_board, j_visited, j_start)
 
-
-print(bfs(board, start, visited, fired))
+print('IMPOSSIBLE')
