72416aa3e0528a1aa672dfeca934be1dc1029c45
lvalentine6 <ysr1227@naver.com>
2025-04-30 22:10:30 +0300

    보물

---
+++ baekjoon/silver/1026.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+answer = 0
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+    answer += a[i] * b[i]
+
+print(answer)
