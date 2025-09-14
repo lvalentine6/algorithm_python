04e5f277a538e240ade087412da2cfa583fb96cc
lvalentine6 <ysr1227@naver.com>
2025-09-15 00:47:56 +0900

    포도주 시식

--- baekjoon/silver/2156.py
+++ baekjoon/silver/2156.py
@@ -1,27 +1,24 @@
 import sys
 
-sys.setrecursionlimit(100000)
 input = sys.stdin.readline
+
 n = int(input())
-lst = [int(input().strip()) for _ in range(n)]
-_max = 0
+wine = [0] * (n + 1)
+for i in range(1, n + 1):
+    wine[i] = int(input().strip())
 
+dp = [0] * (n + 1)
 
-def recursion(lst, _sum, idx, r):
-    global _max
-    if idx > n - 1:
-        _max = max(_sum, _max)
-        return
+if n >= 1:
+    dp[1] = wine[1]
+if n >= 2:
+    dp[2] = wine[1] + wine[2]
 
-    _sum += lst[idx]
+for i in range(3, n + 1):
+    dp[i] = max(
+        dp[i - 1],
+        dp[i - 2] + wine[i],
+        dp[i - 3] + wine[i - 1] + wine[i]
+    )
 
-    if r == 1:
-        recursion(lst, _sum, idx + 2, 0)
-    else:
-        recursion(lst, _sum, idx + 1, r + 1)
-
-
-for i in range(n):
-    recursion(lst, 0, i, 0)
-
-print(_max)
+print(dp[n])
