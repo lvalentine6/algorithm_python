0fa0dc32a1ed1c0473a70f1f974439659c5a0c54
lvalentine6 <ysr1227@naver.com>
2025-09-17 00:09:35 +0900

    feat: 부분수열의 합

--- baekjoon/silver/1182.py
+++ baekjoon/silver/1182.py
@@ -1,24 +1,17 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
-n, s = map(int, input().split())
+n, s = list(map(int, input().split()))
 lst = list(map(int, input().split()))
-
-x, y = 0, 0
-_sum = 0
 answer = 0
 
-while True:
-    if _sum >= s or y == n:
+for i in range(1, n + 1):
+    comb = combinations(lst, i)
+    for j in comb:
+        _sum = sum(j)
         if _sum == s:
             answer += 1
-        _sum -= lst[x]
-        x += 1
-        if x == n:
-            break
-    else:
-        _sum += lst[y]
-        y += 1
 
 print(answer)
